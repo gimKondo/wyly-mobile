d9ef1a7963b4c8249a95ba0180f7c4d9401df381
Kondo Daisuke <gim0kondo@gmail.com>
2020-02-16 23:04:36 +0900

    :green_heart: golden test of main

---
+++ test/golden/golden_widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../lib/main.dart';
+
+void main() {
+  testWidgets('Golden test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await expectLater(find.byType(MyApp), matchesGoldenFile('main.png'));
+  });
+}
