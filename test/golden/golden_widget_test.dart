1da0612bf2308184433dfb927d0d208b958e081d
Kondo Daisuke <gim0kondo@gmail.com>
2020-03-23 01:03:58 +0900

    :green_heart: not fix screen size for golden test

--- test/golden/golden_widget_test.dart
+++ test/golden/golden_widget_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import './screen_size_golden.dart';
 import 'package:wyly/main.dart';
 
 void main() {
   testWidgets('Golden test', (tester) async {
-    await tester.setScreenSize(width: 2160, height: 1080);
     await tester.pumpWidget(MyApp());
     await expectLater(find.byType(MyApp), matchesGoldenFile('main.png'));
   });
