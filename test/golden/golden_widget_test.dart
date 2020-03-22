5b3470e46b66f86ccc8b96a5a61722fa15a0b79e
Kondo Daisuke <gim0kondo@gmail.com>
2020-03-23 01:05:33 +0900

    :green_heart: run golden test only on MacOS
    - golden image is made in MacOS

--- test/golden/golden_widget_test.dart
+++ test/golden/golden_widget_test.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wyly/main.dart';
 
 void main() {
   testWidgets('Golden test', (tester) async {
     await tester.pumpWidget(MyApp());
-    await expectLater(find.byType(MyApp), matchesGoldenFile('main.png'));
+    await expectLater(find.byType(MyApp), matchesGoldenFile('main.png'),
+        skip: !Platform.isMacOS);
   });
 }
