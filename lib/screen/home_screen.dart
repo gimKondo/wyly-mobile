eb10c3538d347eb90e25d139e0ec7216d9ccdef5
Kondo Daisuke <gim0kondo@gmail.com>
2020-04-15 03:29:08 +0900

    :tada: create timeline structure

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../widget/bottom_navigator.dart';
 import '../service/timeline_service.dart';
+import '../widget/timeline_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Wyly')),
-      body: Column(children: [Text('Photo')]),
+      body: TimelineList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () async => _onPressSearchButton(context),
         tooltip: 'Search',
