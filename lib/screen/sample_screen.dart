b2d83b9de220f5f302689e81ae4a6fa51e4c2d0c
Kondo Daisuke <gim0kondo@gmail.com>
2020-04-26 23:05:54 +0900

    :shirt: change old theme name

--- lib/screen/sample_screen.dart
+++ lib/screen/sample_screen.dart
@@ -73,7 +73,7 @@ class _SampleScreenState extends State<SampleScreen> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
