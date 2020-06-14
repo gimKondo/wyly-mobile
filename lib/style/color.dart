6a1438612adc5ed21bf6f3045456a53b9f404931
Kondo Daisuke <gim0kondo@gmail.com>
2020-06-15 01:35:35 +0900

    :recycle: define color as style

---
+++ lib/style/color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// theme colors
+final primaryColor = Color.fromARGB(0xFF, 0x52, 0x33, 0x29); // #523329
+final canvasColor = Color.fromARGB(0xFF, 0xC6, 0xCE, 0xD3); // #C6CED3
+final cardColor = Color.fromARGB(0xFF, 0x9C, 0x9C, 0x82); // #9C9C82
+final disabledColor = Color.fromARGB(0xFF, 0x6D, 0x83, 0x9B); // #6D839B
+final accentColor = Color.fromARGB(0xFF, 0x98, 0x4F, 0x2B); // #984F2B
+
+// other colors
+final plainTextColor = Colors.black;
+final subTextColor = Colors.black54;
+final linkedTextColor = Colors.blueAccent;
+final buttonTextColor = Colors.white;
--- lib/style/text_style.dart
+++ lib/style/text_style.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:wyly/style/color.dart';
+
 const _titleFontSize = 60.0;
 const _mainFontSize = 20.0;
 const _subFontSize = 16.0;
@@ -8,29 +10,29 @@ final titleTextStyle = TextStyle(
   fontSize: _titleFontSize,
   fontWeight: FontWeight.w900,
   fontStyle: FontStyle.italic,
-  color: Colors.brown,
+  color: primaryColor,
 );
 
 final plainTextStyle = TextStyle(
   fontSize: _mainFontSize,
   fontWeight: FontWeight.w500,
-  color: Colors.black,
+  color: plainTextColor,
 );
 
 final linkedPlainTextStyle = TextStyle(
   fontSize: _mainFontSize,
   fontWeight: FontWeight.w500,
-  color: Colors.blueAccent,
+  color: linkedTextColor,
 );
 
 final subPlainTextStyle = TextStyle(
   fontSize: _subFontSize,
   fontWeight: FontWeight.w400,
-  color: Colors.black54,
+  color: subTextColor,
 );
 
 final buttonTextStyle = TextStyle(
   fontSize: _mainFontSize,
   fontWeight: FontWeight.w800,
-  color: Colors.white,
+  color: buttonTextColor,
 );
